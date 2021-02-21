280fd5722ac1b818c82b922add88d5d08b29de77
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-20 19:51:55 -0600

    Agrehando las nuevas vistas

--- App.js
+++ App.js
@@ -1,41 +1,36 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet
-} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
-import { Inicio } from './views/Inicio';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Inicio} from './views/Inicio';
+import {DetallesCliente} from './views/DetallesCliente';
+import {NuevoCliente} from './views/NuevoCliente';
 
 const Stack = createStackNavigator();
-
 
 const App = () => {
   return (
     <>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="Inicio"
-        component={Inicio}
-        > 
-        </Stack.Screen>
-      </Stack.Navigator>
-
-
-    </NavigationContainer>
-     
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Inicio" component={Inicio}>
+            <Stack.Screen
+              name="NuevoCliente"
+              component={NuevoCliente}
+              options={{title: 'Nuevo cliente'}}></Stack.Screen>
+            <Stack.Screen
+              name="DetallesCliente"
+              component={DetallesCliente}
+              options={{title: 'Detalles cliente'}}></Stack.Screen>
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-
-
-});
+const styles = StyleSheet.create({});
 
 export default App;
